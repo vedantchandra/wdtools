0d3acca2252da4d5b9c32e57582f6c6f1ba13e5e
Vedant Chandra <me@vedantchandra.com>
2020-04-20 03:06:19 +0530

    mle initialization for mcfit + new and improved generative NN

--- mcfit.py
+++ mcfit.py
@@ -10,9 +10,9 @@ import sys
 from tqdm import tqdm
 from scipy.interpolate import interp1d
 def find_nearest(array, value):
-	array = np.asarray(array)
-	idx = (np.abs(array - value)).argmin()
-	return idx
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
 import os
 from PyAstronomy.pyasl import dopplerShift
 import emcee
@@ -21,106 +21,216 @@ import corner
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
+import scipy.optimize as opt
+import pyabc
+halpha = 6564.61
+hbeta = 4862.68
+hgamma = 4341.68
+hdelta = 4102.89
 path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
 plt.rcParams.update({'font.size': 16})
 
 class MCFit:
 
-	def __init__(self, resolution = 3):
-		self.H = 32
-		self.reg = 0.0001
-		self.lamgrid = np.arange(4000,8000)
-		self.sc = pickle.load(open(dir_path + '/models/neural_gen/normNN_sc.p', 'rb'))
-		self.msc = pickle.load(open(dir_path + '/models/neural_gen/normNN_msc.p', 'rb'))
-		self.model = self.generator()
-		self.model.load_weights(dir_path + '/models/neural_gen/normNN.h5')
-		self.resolution = resolution
+    def __init__(self, resolution = 3):
+        self.H = 64
+        self.reg = 0.0001
+        self.lamgrid = pickle.load(open(dir_path + '/models/neural_gen/lamgrid.p', 'rb'))
+        self.sc = pickle.load(open(dir_path + '/models/neural_gen/normNN_sc.p', 'rb'))
+        self.msc = pickle.load(open(dir_path + '/models/neural_gen/normNN_msc.p', 'rb'))
+        self.model = self.generator()
+        self.model.load_weights(dir_path + '/models/neural_gen/smallgrid_normNN.h5')
+        self.resolution = resolution
 
 
-	def generator(self):
-		x = Input(shape=(2,))
-		y = Dense(self.H,activation='sigmoid',trainable = True)(x)
-		y = Dense(self.H,activation='sigmoid',trainable = True)(y)
-		out = Dense(4000,activation='linear',trainable = True)(y)
-		
-		model = Model(inputs = x, outputs = out)
-		model.compile(optimizer = Adamax(), loss = 'mse', \
-					  metrics = ['mae'])
-		return model
+    def generator(self):
+        x = Input(shape=(2,))
+        y = Dense(self.H,activation='relu',trainable = True)(x)
+        y = Dense(self.H,activation='relu',trainable = True)(y)
+        out = Dense(len(self.lamgrid),activation='linear',trainable = True)(y)
+        
+        model = Model(inputs = x, outputs = out)
+        model.compile(optimizer = Adamax(), loss = 'mse', \
+                      metrics = ['mae'])
+        return model
 
-	def spectrum_sampler(self, wl, teff, logg, rv):
-		label = self.sc.transform(np.asarray(np.stack((teff,logg)).reshape(1,-1)))
-		synth = dopplerShift(self.lamgrid,np.ravel(
-						self.msc.inverse_transform(
-								self.model.predict(label))[0]
-						), rv
-					)[0]
-		synth =  (np.ravel(synth).astype('float64'))
-		synth = scipy.ndimage.gaussian_filter1d(synth, self.resolution)
-		func = interp1d(self.lamgrid, synth, fill_value = 1, bounds_error = False)
-		return func(wl)
+    def spectrum_sampler(self, wl, teff, logg, rv):
+        label = self.sc.transform(np.asarray(np.stack((teff,logg)).reshape(1,-1)))
+        synth = dopplerShift(self.lamgrid,np.ravel(
+                        (
+                                self.model.predict(label))[0]
+                        ), rv
+                    )[0]
+        synth =  (np.ravel(synth).astype('float64'))
+        synth = scipy.ndimage.gaussian_filter1d(synth, self.resolution)
+        func = interp1d(self.lamgrid, synth, fill_value = 1, bounds_error = False)
+        return func(wl)
 
-	def fit_spectrum(self, wl, fl, ivar, nwalkers = 100, burn = 100, n_draws = 250, make_plot = False):
+    def fit_spectrum(self, wl, fl, ivar, nwalkers = 250, burn = 100, n_draws = 250, make_plot = False, threads = 1, \
+                    plot_trace = False, init = 'unif', prior_teff = None, mleburn = 50):
 
-		def lnlike(prms):
-			model = self.spectrum_sampler(wl,prms[0],prms[1],prms[2])
+        def lnlike(prms):
+            model = self.spectrum_sampler(wl,prms[0],prms[1],prms[2])
 
-			nonan = (~np.isnan(model)) * (~np.isnan(fl)) * (~np.isnan(ivar))
-			diff = model[nonan] - fl[nonan]
-			chisq = np.sum(diff**2 * ivar[nonan])
-			if np.isnan(chisq):
-				return -np.Inf
-			lnlike = -0.5 * chisq
-			return lnlike
+            nonan = (~np.isnan(model)) * (~np.isnan(fl)) * (~np.isnan(ivar))
+            diff = model[nonan] - fl[nonan]
+            chisq = np.sum(diff**2 * ivar[nonan])
+            if np.isnan(chisq):
+                return -np.Inf
+            lnlike = -0.5 * chisq
+            return lnlike
 
-		def lnprior(prms):
-			if prms[0] < 6000 or prms[0] > 80000:
-				return -np.Inf
-			elif prms[1] < 6.5 or prms[1] > 9.5:
-				return -np.Inf
-			elif prms[2] < -300 or prms[2] > 300:
-				return -np.Inf
-			return 0 #np.log(1.0/(np.sqrt(2*np.pi)*sigma))-0.5*(prms[0]-mu)**2/sigma**2
+        def lnprior(prms):
+            if prms[0] < 6000 or prms[0] > 80000:
+                return -np.Inf
+            elif prms[1] < 6.5 or prms[1] > 9.5:
+                return -np.Inf
+            elif prms[2] < -300 or prms[2] > 300:
+                return -np.Inf
+            if prior_teff is not None:
+                mu,sigma = prior_teff
+                return np.log(1.0/(np.sqrt(2*np.pi)*sigma))-0.5*(prms[0]-mu)**2/sigma**2
+            else:
+                return 0
 
-		def lnprob(prms):
-			lp = lnprior(prms)
-			if not np.isfinite(lp):
-				return -np.Inf
-			return lp + lnlike(prms)
+        def lnprob(prms):
+            lp = lnprior(prms)
+            if not np.isfinite(lp):
+                return -np.Inf
+            return lp + lnlike(prms)
+        ndim = 3
+        pos0 = np.zeros((nwalkers,ndim))
 
-		ndim = 3
-		pos0 = np.zeros((nwalkers,ndim))
+        lows = [6500,6.6,-300]
+        highs = [80000,9.4,300]
 
-		lows = [6000,6.6,-300]
-		highs = [80000,9.4,300]
+        sampler = emcee.EnsembleSampler(nwalkers,ndim,lnprob,threads = threads)
 
-		for jj in range(ndim):
-			pos0[:,jj] = ( np.random.uniform(lows[jj], highs[jj], nwalkers) )
+        if init == 'opt':
+            nll = lambda *args: -lnprob(*args)
+            result = opt.minimize(nll, [12000, 8, 0], method = 'Nelder-Mead')
 
-		sampler = emcee.EnsembleSampler(nwalkers,ndim,lnprob,threads = 1)
+            for jj in range(ndim):
+                pos0[:,jj] = (result.x[jj] + 0.001*np.random.normal(size = nwalkers))
 
-		#Initialize sampler
-		print('Initial burn...')
-		b = sampler.run_mcmc(pos0,burn)
+        elif init == 'unif':
+            for jj in range(ndim):
+                pos0[:,jj] = np.random.uniform(lows[jj], highs[jj], nwalkers)
+        elif init == 'mle':
+            for jj in range(ndim):
+                pos0[:,jj] = np.random.uniform(lows[jj], highs[jj], nwalkers)
 
-		sampler.reset()
+            b = sampler.run_mcmc(pos0, mleburn, progress = True)
+            lnprobs = sampler.get_log_prob(flat = True)
+            mle = sampler.flatchain[np.argmax(lnprobs)]
 
-		b = sampler.run_mcmc(b.coords, n_draws)
+            for jj in range(ndim):
+                pos0[:,jj] = (mle[jj] + 0.001*np.random.normal(size = nwalkers))
 
-		medians = np.median(sampler.flatchain, 0)
+            sampler.reset()
 
-		if make_plot:
-			fig,ax = plt.subplots(3,3, figsize = (10,10))
-			f = corner.corner(sampler.flatchain, labels = ['$T_{eff}$', '$\log{g}$', 'RV'], \
-                  fig = fig, show_titles = True, title_kwargs = dict(fontsize = 16), range = (0.99, 0.99, 0.99),\
+
+        #Initialize sampler
+        b = sampler.run_mcmc(pos0,burn, progress = True)
+
+        sampler.reset()
+
+        b = sampler.run_mcmc(b.coords, n_draws, progress = True)
+
+        if plot_trace:
+            f, axs = plt.subplots(3, 1, figsize = (10, 6))
+            for jj in range(ndim):
+                axs[jj].plot(sampler.chain[:,:,jj].T, alpha = 0.3, color = 'k');
+            plt.show()
+
+        lnprobs = sampler.get_log_prob(flat = True)
+        medians = np.median(sampler.flatchain, 0)
+        mle = sampler.flatchain[np.argmax(lnprobs)]
+        fit_fl = self.spectrum_sampler(wl, mle[0], mle[1], mle[2])
+
+        if make_plot:
+            fig,ax = plt.subplots(3,3, figsize = (10,10))
+            f = corner.corner(sampler.flatchain, labels = ['$T_{eff}$', '$\log{g}$', 'RV'], \
+                  fig = fig, show_titles = True, title_kwargs = dict(fontsize = 16),\
                      label_kwargs = dict(fontsize = 16), quantiles = (0.16, 0.5, 0.84))
-			plt.tight_layout()
-			plt.show()
+            plt.tight_layout()
+            plt.show()
 
-			plt.figure(figsize = (10,5))
+            plt.figure(figsize = (8,5))
 
-			plt.plot(wl, fl, 'k')
-			plt.plot(wl, self.spectrum_sampler(wl, medians[0], medians[1], medians[2]), 'r')
+            breakpoints = np.nonzero(np.diff(wl) > 5)[0]
+            breakpoints = np.concatenate(([0], breakpoints, [None]))
 
-		return sampler.flatchain
+            for kk in range(len(breakpoints) - 1):
+                wl_seg = wl[breakpoints[kk] + 1:breakpoints[kk+1]]
+                fl_seg = fl[breakpoints[kk] + 1:breakpoints[kk+1]]
+                fit_fl_seg = fit_fl[breakpoints[kk] + 1:breakpoints[kk+1]]
+                peak = np.argmin(fl_seg)
+                delta_wl = wl_seg - wl_seg[peak]
+                plt.plot(delta_wl, 1 + fl_seg - 0.35 * kk, 'k')
+                plt.plot(delta_wl, 1 + fit_fl_seg - 0.35 * kk, 'r')
+            plt.xlabel(r'$\mathrm{\Delta \lambda}\ (\mathrm{\AA})$')
+            plt.ylabel('Normalized Flux')
+            plt.show()
+
+            plt.figure(figsize = (10,5))
+            plt.plot(wl, fl, 'k')
+            plt.plot(wl, fit_fl, 'r')
+            plt.show()
+            return sampler
+
+
+    def fit_spectrum_abc(self, wl, fl, ivar = None, make_plot = False, popsize = 100, max_pops = 25):
+        if ivar is None:
+            ivar = 1
+
+        obs = dict(spec = fl)
+
+        def sim(params):
+            fl_sample = self.spectrum_sampler(wl, params['teff'], params['logg'], params['rv'])
+            return dict(spec = fl_sample)
+
+        def distance(sim1, sim2):
+            resid = sim1['spec'] - sim2['spec']
+            chisq = np.nansum(resid**2 * ivar)
+            return chisq
+
+        priors = pyabc.Distribution(teff = pyabc.RV("uniform", 6000, 74000),
+                                       logg = pyabc.RV("uniform", 6.5, 3),
+                                       rv = pyabc.RV("uniform", -300, 600))
+
+        abc = pyabc.ABCSMC(sim, priors,\
+                           distance, sampler = pyabc.sampler.SingleCoreSampler(),\
+                           population_size = popsize, \
+                           eps = pyabc.epsilon.QuantileEpsilon(alpha = 0.5))
+
+        db = ("sqlite:///mcfit.db")
+        abc.new(db, obs)
+
+        history = abc.run(min_acceptance_rate = 0.01, max_nr_populations = max_pops)
+
+        if make_plot:
+            pyabc.visualization.plot_kde_matrix_highlevel(history, height = 4)
+            plt.show()
+
+            df, w = history.get_distribution()
+            plt.figure(figsize = (8,5))
+            medians = df.median()
+            fit_fl = self.spectrum_sampler(wl, medians['teff'], medians['logg'], medians['rv'])
+            breakpoints = np.nonzero(np.diff(wl) > 5)[0]
+            breakpoints = np.concatenate(([0], breakpoints, [None]))
+
+            for kk in range(len(breakpoints) - 1):
+                wl_seg = wl[breakpoints[kk] + 1:breakpoints[kk+1]]
+                fl_seg = fl[breakpoints[kk] + 1:breakpoints[kk+1]]
+                fit_fl_seg = fit_fl[breakpoints[kk] + 1:breakpoints[kk+1]]
+                peak = np.argmin(fl_seg)
+                delta_wl = wl_seg - wl_seg[peak]
+                plt.plot(delta_wl, 1 + fl_seg - 0.35 * kk, 'k')
+                plt.plot(delta_wl, 1 + fit_fl_seg - 0.35 * kk, 'r')
+            plt.xlabel(r'$\mathrm{\Delta \lambda}\ (\mathrm{\AA})$')
+            plt.ylabel('Normalized Flux')
+            plt.show()
+
+        return history
--- spectrum.py
+++ spectrum.py
@@ -10,12 +10,12 @@ import os
 plt.rcParams.update({'font.size': 18})
 path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
-import pwlf
 import scipy
-import csaps
 from PyAstronomy.pyasl import crosscorrRV, quadExtreme, dopplerShift
 from scipy.interpolate import interp1d
 import lmfit
+from lmfit.models import LinearModel, VoigtModel, GaussianModel
+speed_light = 299792458 #m/s
 
 class SpecTools():
 
@@ -34,36 +34,89 @@ class SpecTools():
 		self.hbeta = 4862.68
 		self.hgamma = 4341.68
 		self.hdelta = 4102.89
+		linear_model = LinearModel(prefix = 'l_')
+		self.params = linear_model.make_params()
+		voigt_model = VoigtModel(prefix = 'v_')
+		self.params.update(voigt_model.make_params())
+		self.cm = linear_model - voigt_model
+		self.params['v_amplitude'].set(value = 150)
+		self.params['v_sigma'].set(value = 5)
+		self.params['l_intercept'].set(value = 25)
+		self.params['l_slope'].set(value = 0)
 
-	def continuum_normalize(self, wl, flux):
-		pixels = wl - 4000
-		continuum = (pixels > 2900) + (pixels < 2200)*(pixels > 1050) + (pixels < 650)*(pixels > 530) \
-		+ (pixels < 220)*(pixels > 200) + (pixels < 35)*(pixels > 20) + (pixels < -200)*(pixels > -450)
+	def normalize_line(self, wl, fl, ivar, centroid, distance, make_plot = False):
 
-		notnan = (~np.isnan(flux))+(~np.isnan(wl))
-		skylinemask = (wl > 5578.5 - 10)*(wl < 5578.5 + 10) + (wl > 5894.6 - 10)*(wl < 5894.6 + 10)\
-		+ (wl > 6301.7 - 10)*(wl < 6301.7 + 10) + (wl > 7246.0 - 10)*(wl < 7246.0 + 10)
+		self.params['v_center'].set(value = centroid)
 
-		#spl = scipy.interpolate.UnivariateSpline(wl[continuum*notnan], flux[continuum*notnan], k = 1, s = self.smoothing)
+		crop1 = bisect_left(wl, centroid - distance)
+		crop2 = bisect_left(wl, centroid + distance)
 
-		spl = csaps.UnivariateCubicSmoothingSpline(wl[continuum*notnan], flux[continuum*notnan], smooth = self.smoothing)
+		cropped_wl = wl[crop1:crop2]
+		cropped_fl = fl[crop1:crop2]
+		
+		res = self.cm.fit(cropped_fl, self.params, x = cropped_wl)
+		slope = res.params['l_slope']
+		intercept = res.params['l_intercept']
+		
+		if make_plot:
+			plt.plot(cropped_wl, cropped_fl)
+			#plt.plot(cropped_wl, self.cm.eval(params, x=cropped_wl))
+			plt.plot(cropped_wl, res.eval(res.params, x=cropped_wl))
+			plt.plot(cropped_wl, cropped_wl*slope + intercept)
+			plt.show()
+		
+		continuum = (slope * cropped_wl + intercept)
+		
+		fl_normalized = cropped_fl / continuum
+		
+		if ivar is not None:
+			cropped_ivar = ivar[crop1:crop2]
+			ivar_normalized = cropped_ivar * continuum**2
+			return cropped_wl, fl_normalized, ivar_normalized
+		else:
+			return cropped_wl, fl_normalized
 
-		cont = spl(wl)
-		contcorr = flux / cont
-
-		if self.filter_skylines:
-			contcorr[skylinemask] = 1
-
-		if self.crop:
-			where = (contcorr > 1.5) + (contcorr < 0)
-			contcorr[where] = 1
-
-		if self.plot_continuum == True:
-			plt.figure()
-			plt.plot(wl,flux)
-			plt.plot(wl,cont)
-
-		return contcorr
+	def normalize_balmer(self, wl, fl, ivar = None, lines = ['alpha', 'beta', 'gamma', 'delta'], \
+						 skylines = True, make_plot = False, make_subplot = False, make_stackedplot = False, \
+							 centroid_dict = dict(alpha = 6564.61, beta = 4862.68, gamma = 4341.68, delta = 4102.89),
+								distance_dict = dict(alpha = 250, beta = 250, gamma = 130, delta = 100)):
+		
+		fl_normalized = [];
+		wl_normalized = [];
+		ivar_normalized = [];
+		ct = 0;
+		
+		centroid_dict = dict(alpha = 6564.61, beta = 4862.68, gamma = 4341.68, delta = 4102.89)
+		distance_dict = dict(alpha = 300, beta = 200, gamma = 120, delta = 90)
+		
+		
+		for line in lines:
+			if ivar is not None:
+				wl_segment, fl_segment, ivar_segment = self.normalize_line(wl, fl, ivar, centroid_dict[line], distance_dict[line], make_plot = make_subplot)
+				fl_normalized = np.append(fl_segment, fl_normalized)
+				wl_normalized = np.append(wl_segment, wl_normalized)
+				ivar_normalized = np.append(ivar_segment, ivar_normalized)
+				
+			else:
+				wl_segment, fl_segment = self.normalize_line(wl, fl, None, centroid_dict[line],\
+														distance_dict[line], make_plot = make_subplot)
+				plt.show()
+				fl_normalized = np.append(fl_segment, fl_normalized)
+				wl_normalized = np.append(wl_segment, wl_normalized)
+				
+		if skylines:
+			skylinemask = (wl_normalized > 5578.5 - 10)*(wl_normalized < 5578.5 + 10) + (wl_normalized > 5894.6 - 10)\
+			*(wl_normalized < 5894.6 + 10) + (wl_normalized > 6301.7 - 10)*(wl_normalized < 6301.7 + 10) + \
+			(wl_normalized > 7246.0 - 10)*(wl_normalized < 7246.0 + 10)
+			fl_normalized[skylinemask] = 1
+		
+		if make_plot:
+			plt.plot(wl_normalized, fl_normalized, 'k')
+			
+		if ivar is not None:
+			return wl_normalized, fl_normalized, ivar_normalized
+		else:
+			return wl_normalized, fl_normalized
 
 	def find_nearest(self,array, value):
 		array = np.asarray(array)
@@ -118,64 +171,97 @@ class RVTools():
 
 		return np.sum(residual**2)
 
-	def find_centroid(self, wl, flux, centroid, half_window = 25, window_step = 25, n_fit = 4, make_plot = False):
-
-		in1 = bisect_left(wl,centroid-150)
-		in2 = bisect_left(wl,centroid+150)
+	def find_centroid(self, wl, flux, centroid, half_window = 25, window_step = 2, n_fit = 12, make_plot = False, \
+				 pltname = ''):
+	
+		window_step = -window_step
+		in1 = bisect_left(wl,centroid-60)
+		in2 = bisect_left(wl,centroid+60)
 		cropped_wl = wl[in1:in2]
 		cflux = flux[in1:in2]
 
-		cmask = (cropped_wl < centroid - 75)+(cropped_wl > centroid + 75)
+	#     plt.plot(cropped_wl, cflux)
+		
+		cmask = (cropped_wl < centroid - 50)+(cropped_wl > centroid + 50)
 
-		p,cov = curve_fit(self.linear,cropped_wl[cmask],cflux[cmask])
+		p,cov = curve_fit(self.linear,cropped_wl[cmask][~np.isnan(cflux[cmask])],cflux[cmask][~np.isnan(cflux[cmask])])
 
-		contcorr = cflux / self.linear(cropped_wl, p[0], p[1])
-
-
-		linemodel = lmfit.models.VoigtModel()
+		contcorr = cflux / self.linear(cropped_wl, *p)
+		linemodel = lmfit.models.GaussianModel()
 		params = linemodel.make_params()
-		params['amplitude'].set(value = 10)
+		params['amplitude'].set(value = 2)
 		params['center'].set(value = centroid)
-		params['sigma'].set(value=5)
+		params['sigma'].set(value=2)
 		centres = [];
 		errors = [];
-
-		if make_plot:
-			plt.figure(figsize=(10,5))
-			plt.title(str(centroid)+"$\AA$")
-			plt.plot(cropped_wl,contcorr,'k')
-
-		for ii in range(n_fit):
 		
-			crop1 = bisect_left(cropped_wl,centroid - half_window - ii*window_step)
-			crop2 = bisect_left(cropped_wl,centroid + half_window + ii*window_step)
+		if make_plot:
+			plt.figure(figsize = (10, 5))
+			#plt.title(str(centroid)+"$\AA$")
+			plt.plot(cropped_wl,contcorr,'k')
+			#plt.plot(cropped_wl, 1-linemodel.eval(params, x = cropped_wl))
+		
+		crop1 = bisect_left(cropped_wl,centroid - 15)
+		crop2 = bisect_left(cropped_wl,centroid + 15)
+		init_result = linemodel.fit(1-contcorr[crop1:crop2],params,x = cropped_wl[crop1:crop2],\
+							nan_policy = 'omit',method='lm')
+		
+		adaptive_centroid = init_result.params['center'].value
+		
+		for ii in range(n_fit):
+			
+			crop1 = bisect_left(cropped_wl, adaptive_centroid - half_window - ii*window_step)
+			crop2 = bisect_left(cropped_wl, adaptive_centroid + half_window + ii*window_step)
+			
 			try:
 				result = linemodel.fit(1-contcorr[crop1:crop2],params,x = cropped_wl[crop1:crop2],\
-				nan_policy = 'omit',method='dual_annealing',fit_kws={'reduce_fcn':self.chisquare})
+							nan_policy = 'omit')
+				if np.abs(result.params['center'].value - adaptive_centroid) > 5:
+					continue
 			except ValueError:
-				print('fit failed! returning NaN')
-				return np.nan,np.nan
-
-			centres.append(result.params['center'].value)
-			errors.append(result.params['center'].stderr)
-
+	#             print('one fit failed. skipping...')
+				continue
+				
+			if ii != 0:
+				centres.append(result.params['center'].value)
+				errors.append(result.params['center'].stderr)
+			
+			adaptive_centroid = result.params['center'].value
+			
+	#         print(len(cropped_wl[crop1:crop2]))
 			if make_plot:
-				plt.plot(cropped_wl[crop1:crop2],1-linemodel.eval(result.params,x=cropped_wl[crop1:crop2]),'r', linewidth = 1)
-				#plt.plot(cropped_wl[crop1:crop2],1-linemodel.eval(params,x=cropped_wl[crop1:crop2]),'k--')
-
+				xgrid = np.linspace(cropped_wl[crop1:crop2][0], cropped_wl[crop1:crop2][-1], 1000)
+				
+				plt.plot(xgrid,1-linemodel.eval(result.params, x = xgrid),\
+						 'r', linewidth = 1, alpha = 0.7)
+	#            plt.plot(cropped_wl[crop1:crop2],1-linemodel.eval(params,x=cropped_wl[crop1:crop2]),'k--')
+		
 		mean_centre = np.mean(centres)
 		sigma_sample = np.std(centres)
-		#print(np.isnan(np.array(errors)))
-		if None in errors or np.nan in errors:
-			return mean_centre, np.nan
-
-		sigma_propagated = np.linalg.norm(errors) / n_fit
-
-		total_sigma = np.sqrt(sigma_propagated**2 + sigma_sample**2)
-
+		sigma_propagated = np.median(errors)
+		total_sigma = np.sqrt(sigma_propagated**2 + sigma_sample**2) 
+		
 		if make_plot:
-			plt.xlabel('Wavelength ($\AA$)')
-			plt.ylabel('Normalized Flux')
-			plt.show()
-
-		return mean_centre, total_sigma
+	#         gap = (50*1e-5)*centroid
+	#         ticks = np.arange(centroid - gap*4, centroid + gap*4, gap)
+	#         rvticks = ((ticks - centroid) / centroid)*3e5
+	#         plt.xticks(ticks, np.round(rvticks).astype(int))
+			plt.xlabel('Wavelength ($\mathrm{\AA}$)')
+			plt.ylabel('Flux (Normalized)')
+			plt.xlim(centroid - 35,centroid + 35)
+			plt.axvline(centroid, color = 'k', linestyle = '--')
+			plt.axvline(mean_centre, color = 'r', linestyle = '--')
+			plt.tick_params(bottom=True, top=True, left=True, right=True)
+			plt.text(0.65, 0.1, '$\mathrm{v_r}$ = %i ± %i km/s'\
+					 %(speed_light*1e-3*(mean_centre - centroid)/centroid, speed_light*1e-3*(sigma_sample/centroid)),\
+					fontsize = 22, transform = plt.gca().transAxes)
+			#plt.xlim(adaptive_centroid - 10, adaptive_centroid + 10)
+			plt.minorticks_on()
+			plt.tick_params(which='major', length=10, width=1, direction='in', top = True, right = True)
+			plt.tick_params(which='minor', length=5, width=1, direction='in', top = True, right = True)
+			plt.tight_layout()
+		if None in errors or np.nan in errors:
+			return mean_centre, np.nan, sigma_sample
+			#print(np.isnan(np.array(errors)))
+			
+		return mean_centre, sigma_propagated, sigma_sample
