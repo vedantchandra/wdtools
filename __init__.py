214f4aaf03cf95971f6263ce668819d9e26b2e49
Vedant Chandra <me@vedantchandra.com>
2020-06-17 01:09:46 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-import wdtools
+raise Exception('oops, you seem to have imported the wdtools/ repository rather than the wdtools/wdtools Python package! kindly use sys.path.append to add the parents repository directory to your path and then import wdtools.')
