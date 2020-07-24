400ddc4b2b93a5a39d623326928e27afbc121ab2
Vedant Chandra <me@vedantchandra.com>
2020-07-24 17:52:50 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2020, Vedant Chandra'
 author = 'Vedant Chandra'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
